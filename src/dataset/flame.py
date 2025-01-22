2ed2a33b3e1f5b201670b532fffec8ee82c22cf9
ghazafm <fauzan.ghaza@gmail.com>
2025-01-22 16:32:18 +0700

    fix(dataset): Add url to flame dataset

--- src/dataset/flame.py
+++ src/dataset/flame.py
@@ -4,10 +4,10 @@ from collections.abc import Callable
 from dataset.base import BaseDataset
 
 
-class Albi(BaseDataset):
+class FlameRGB(BaseDataset):
     def __init__(
         self,
-        root: str | Path = "../data/albi",
+        root: str | Path = "../data/flame_rgb",
         transform: Callable | None = None,
         download: bool = False,
         urls: dict[str, str] | None = None,
@@ -22,9 +22,54 @@ class Albi(BaseDataset):
     # pylint: disable=duplicate-code
     def get_default_urls(self) -> dict[str, str]:
         return {
-            "data_url": "https://drive.google.com/file/d/1lEmbEJHXVn7z80fYevjMVdqBo4FdwLpP/view?usp=drive_link",
-            "annotation_url": "https://drive.google.com/file/d/1cVCm9gTWR1ovbhb1lpO8Fx7NqqO7kIFb/view?usp=drive_link",
+            "data_url": "https://drive.google.com/file/d/1BlteS5qxbBHIVjppHm8C7yp2PomQjdfl/view?usp=sharing",
+            "annotation_url": "",
         }
+    # pylint: enable=duplicate-code
 
 
-# pylint: enable=duplicate-code
+class FlameFOV(BaseDataset):
+    def __init__(
+        self,
+        root: str | Path = "../data/flame_thermal",
+        transform: Callable | None = None,
+        download: bool = False,
+        urls: dict[str, str] | None = None,
+    ):
+        super().__init__(
+            root=root,
+            transform=transform,
+            download=download,
+            urls=urls,
+        )
+
+    # pylint: disable=duplicate-code
+    def get_default_urls(self) -> dict[str, str]:
+        return {
+            "data_url": "https://drive.google.com/file/d/1VeHGZeunP1Hq_Qvf31MvB5FbL0CB5ZvV/view?usp=sharing",
+            "annotation_url": "",
+        }
+    # pylint: enable=duplicate-code
+
+class FlameThermal(BaseDataset):
+    def __init__(
+        self,
+        root: str | Path = "../data/flame_thermal",
+        transform: Callable | None = None,
+        download: bool = False,
+        urls: dict[str, str] | None = None,
+    ):
+        super().__init__(
+            root=root,
+            transform=transform,
+            download=download,
+            urls=urls,
+        )
+
+    # pylint: disable=duplicate-code
+    def get_default_urls(self) -> dict[str, str]:
+        return {
+            "data_url": "https://drive.google.com/file/d/1vx6zqxoaCS5eqVu3AqbjiWhp4uTa5_16/view?usp=sharing",
+            "annotation_url": "",
+        }
+    # pylint: enable=duplicate-code
