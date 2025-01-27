1b4bba9f21ce14242cf96e0d288469c5875136b9
ghazafm <fauzan.ghaza@gmail.com>
2025-01-27 16:58:45 +0700

    feat(preprocessing): add preprocessing for image padder

--- src/helper/image_processing.py
+++ src/helper/image_processing.py
@@ -1,7 +1,22 @@
 from collections.abc import Callable
 from PIL import Image
+from torchvision.transforms import functional as F
 
 
 def transform_image(img: Image, transform: Callable) -> Image:
     """Apply the image transformation."""
     return transform(img)
+
+
+def calculate_padding(image):
+    w, h = image.size
+    max_wh = max(w, h)
+    pad_w = (max_wh - w) // 2
+    pad_h = (max_wh - h) // 2
+    return (pad_w, pad_h, max_wh - w - pad_w, max_wh - h - pad_h)
+
+
+class SquarePadTransform:
+    def __call__(self, image):
+        padding = calculate_padding(image)
+        return F.pad(image, padding, fill=0, padding_mode="constant")
